7c1392304dea9068e2293425c75c972d9d5a96f4
THREADRIPPER <ijlalahmad845@gmail.com>
2022-03-03 18:51:39 +0530

    Time: 4 ms (97.53%), Space: 5.9 MB (75.66%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+              
+        if(x<0)
+            return false;
+        
+        long n=x;
+        long rev=0;
+        while(x!=0)
+        {
+            rev=rev*10+x%10;
+            x/=10;
+        }
+        
+
+        return n==rev;
+    }
+};
