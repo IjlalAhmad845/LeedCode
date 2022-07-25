91ae2ab48b64f8b2741912e0ead3479bdc010770
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-07-25 20:32:57 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (25.00%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        set<char> st;
+        
+        for(int i=0;i<s.size();i++)
+        {
+            if(st.find(s[i])!=st.end())
+                return s[i];
+            else 
+                st.insert(s[i]);
+        }
+        
+        return '0';
+    }
+};
