267e4c076f94746b8efe38cf07dff9baafdd2fcb
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-09-11 15:46:30 +0530

    Time: 352 ms (41.14%), Space: 10.2 MB (93.02%) - LeetHub

---
+++ 204-count-primes/204-count-primes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        vector<bool> sieve(n+1,true);
+        
+        int count=0;
+        for(int i=2;i<n;i++)
+        {
+            //dont check for numbers which are already declared non-prime
+            if(sieve[i])
+            {
+                count++;
+                //if(i*i<n)
+                for(int j=2*i;j<n;j+=i)
+                {
+                    sieve[j]=false;
+                }
+            }
+        
+        }
+        
+        
+        return count;
+    }
+    
+    
+};
