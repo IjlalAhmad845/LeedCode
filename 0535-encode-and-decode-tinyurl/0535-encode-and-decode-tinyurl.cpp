ce4c0ec941826450eb90890dfeb096ae6ad126d8
Ijlal Ahmad <ijlalahmad845@gmail.com>
2022-10-16 13:43:14 +0530

    Time: 3 ms (88.53%), Space: 7 MB (51.35%) - LeetHub

---
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    map<string,string> m;
+    int i=0;
+
+    // Encodes a URL to a shortened URL.
+    string encode(string longUrl) {
+        m["http://tinyurl.com/"+to_string(i)]=longUrl;
+        i++;
+        return "http://tinyurl.com/"+to_string(i-1);
+    }
+
+    // Decodes a shortened URL to its original URL.
+    string decode(string shortUrl) {
+        cout<<shortUrl;
+        return m[shortUrl];
+    }
+};
+
+// Your Solution object will be instantiated and called as such:
+// Solution solution;
+// solution.decode(solution.encode(url));
